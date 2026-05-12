d8593868ad7db125cd1d1c5dae9abafec374b57e
Dakota Kimble-Curry <145517847+iTechDMV@users.noreply.github.com>
2026-05-12 00:09:32 -0400

    Add Home component with multiple subcomponents

---
+++ Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import WhyUs from "../components/WhyUs";
+import SystemMap from "../components/SystemMap";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <WhyUs />
+      <SystemMap />
+      <Contact />
+    </>
+  );
+}
